96c270a844914f0bf51e6f8f0ed5a46122a847cc
admin <admin@VGC-JS74FB>
2017-01-11 00:59:59 +0900

    プロジェクト ファイルを追加します。

---
+++ ClipBoradTest/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ClipBoradTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clipboard.GetText";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(23, 369);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(343, 353);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 310);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "textBox1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 354);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "RichTextBox1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(536, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Clipboard.Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(372, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 45);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Clipboard.GetDataObject";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(372, 129);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 45);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "DataFormatsのフィールド";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(565, 318);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 756);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ClipBoradTest/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipBoradTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // クリップボードに文字列データがあるか確認
+            if (Clipboard.ContainsText())
+            {
+                //文字列データを取得する
+                textBox1.Clear();
+                textBox1.Text = Clipboard.GetText();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //クリップボードのデータを取得する
+            //クリップボードにデータが無いときはnullを返す
+            IDataObject data = Clipboard.GetDataObject();
+
+            if (data != null)
+            {
+                textBox1.Clear();
+                //関連付けられているすべての形式を列挙する
+                foreach (string fmt in data.GetFormats(false))
+                {
+                    textBox1.AppendText(fmt + "\r\n");
+
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.AppendText("DataFormats.Bitmap = " + DataFormats.Bitmap + "\r\n");
+            textBox1.AppendText("DataFormats.CommaSeparatedValue = " + DataFormats.CommaSeparatedValue + "\r\n");
+            textBox1.AppendText("DataFormats.Dib = " + DataFormats.Dib + "\r\n");
+            textBox1.AppendText("DataFormats.Dif = " + DataFormats.Dif + "\r\n");
+            textBox1.AppendText("DataFormats.EnhancedMetafile = " + DataFormats.EnhancedMetafile + "\r\n");
+            textBox1.AppendText("DataFormats.FileDrop = " + DataFormats.FileDrop + "\r\n");
+            textBox1.AppendText("DataFormats.Html = " + DataFormats.Html + "\r\n");
+            textBox1.AppendText("DataFormats.Locale = " + DataFormats.Locale + "\r\n");
+            textBox1.AppendText("DataFormats.MetafilePict = " + DataFormats.MetafilePict + "\r\n");
+            textBox1.AppendText("DataFormats.OemText = " + DataFormats.OemText + "\r\n");
+            textBox1.AppendText("DataFormats.Palette = " + DataFormats.Palette + "\r\n");
+            textBox1.AppendText("DataFormats.PenData = " + DataFormats.PenData + "\r\n");
+            textBox1.AppendText("DataFormats.Riff = " + DataFormats.Riff + "\r\n");
+            textBox1.AppendText("DataFormats.Rtf = " + DataFormats.Rtf + "\r\n");
+            textBox1.AppendText("DataFormats.Serializable = " + DataFormats.Serializable + "\r\n");
+            textBox1.AppendText("DataFormats.StringFormat = " + DataFormats.StringFormat + "\r\n");
+            textBox1.AppendText("DataFormats.SymbolicLink = " + DataFormats.SymbolicLink + "\r\n");
+            textBox1.AppendText("DataFormats.Text = " + DataFormats.Text + "\r\n");
+            textBox1.AppendText("DataFormats.Tiff = " + DataFormats.Tiff + "\r\n");
+            textBox1.AppendText("DataFormats.UnicodeText = " + DataFormats.UnicodeText + "\r\n");
+            textBox1.AppendText("DataFormats.WaveAudio = " + DataFormats.WaveAudio + "\r\n");
+
+        }
+    }
+}
